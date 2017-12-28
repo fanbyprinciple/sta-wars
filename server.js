d28d331f4f787e879bdd36945569fdf5cce0f01c
fanbyprinciple <fanbyprinciple@gmail.com>
2017-12-29 02:34:25 +0530

    integrated Mongodb

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require ('express');
 const bodyParser = require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient
+
+var db;
+
+MongoClient.connect('your-mongodb-url', (err, database) => {
+  if (err) return console.log(err);
+  db = database;
+  app.listen(3000, () => {
+    console.log('listening on 3000');
+  });
+});
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -8,11 +19,14 @@ app.get('/', function (reqS,res){
     res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/quotes', (req,res)=>{
-    console.log("Well hello !")
-});
+app.post('/quotes', (req, res) => {
+    db.collection('quotes').save(req.body, (err, result) => {
+      if (err) return console.log(err)
+  
+      console.log('saved to database')
+      res.redirect('/')
+    });
+  });
 
-app.listen(3000, function() {
-    console.log("Listening on 3000");
-});
+
 
